352e427f50463679bf77924e6dcb63fc38f3fa06
tahiamubin <tahia.mubin1@gmail.com>
2026-04-14 11:15:52 +0600

    designs completed

--- src/components/HomePage/Homepage.jsx
+++ src/components/HomePage/Homepage.jsx
@@ -3,19 +3,22 @@ import Banner from './Banner';
 import Count from './Count';
 import FriendsCards from '../ui/FriendsCards';
 import FriendsDetails from '../ui/FriendsDetails';
+import Timeline from '../Timeline/Timeline';
 
 const Homepage = () => {
     return (
         <div className='container mx-auto'>
             <Banner></Banner>
             <Count></Count>
+            {/* Heading */}
             <div>
                 <h1 className='text-3xl text-gray-700 font-bold'
                 >Your Friends</h1>
             </div>
             <FriendsCards></FriendsCards>
             
-            <FriendsDetails></FriendsDetails>
+            
+           
         </div>
     );
 };
--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -1,9 +1,22 @@
+import { Handshake } from 'lucide-react';
 import React from 'react';
 
 const Timeline = () => {
     return (
-        <div>
-            <h1>Timeline</h1>
+        <div className='container mx-auto space-y-6'>
+            <h1 className='text-4xl font-bold text-gray-700 '>Timeline</h1>
+             {/* sort */}
+
+            <div className='rounded-xl '>
+                 <div className="card w-full bg-base-100 card-xs shadow-sm p-2">
+  <div className="card-body">
+    <h2 className="card-title"><Handshake /></h2>
+    <p className='text-[#244D3F] text-2xl'><span className='font-bold'>Meetup</span> <span>with Enter your name</span></p>
+    <p className='text-gray-800 text-xl'>March 21, 2026</p>
+    
+  </div>
+</div>
+            </div>
         </div>
     );
 };
